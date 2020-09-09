a36b51be8be1042c43b6acc4a22e9b4c45994cc6
EleacastBim <eleacastbim@gmail.com>
2020-09-09 15:04:05 -0300

    ejemplo para mi hermano

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import React, {useState, useEffect} from "react";
-
+import Reloj from './Reloj';
 export default function App() {
-  const [time, setTime] = useState(new Date().toString());
   
-  useEffect(()=>{
-    setTimeout(()=>{
-      setTime(new Date().toString());
-    },1000);
-  })
   return (
-    <div className = "container">
-      <div className="card text-white bg-success mb-2"> 
-          <div className="card-body">
-          <p className="card-text">{time}</p>
-          </div>
-      </div>
+    <div>
+    <Reloj/>
+    <Reloj/>
+    <Reloj/>
+    <Reloj/>
     </div>
-    
   );
+
+  
+
+
+
+
 }
---
+++ src/Reloj.js
@@ -0,0 +1,24 @@
+import React, {useState, useEffect} from "react";
+
+export default function Reloj() {
+  const [time, setTime] = useState(new Date().toString());
+  
+  useEffect(()=>{
+    setTimeout(()=>{
+      setTime(new Date().toString());
+    },1000);
+  })
+  return (
+    <div className = "container">
+      <div className="card text-white bg-success mb-2"> 
+          <div className="card-body">
+          <p className="card-text">{time}</p>
+          </div>
+      </div>
+    </div>
+    
+  );
+}
+
+
+
